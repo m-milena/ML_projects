c224f9c86a7e3d57f373859e3cdb94993e0634c1
m-milena <milenamolska@gmail.com>
2020-04-04 14:00:02 +0200

    Added function for loading dataset from users path

--- poznan_buildings_classification/load_dataset.py
+++ poznan_buildings_classification/load_dataset.py
@@ -1,10 +1,24 @@
 import os
+import sys
 import cv2
+import numpy as np
 
-dataset_path = './dataset/'
+"""Function for loading dataset from users path
+   - load imgs from path, when pictures are gruped in subfolders and based on this subfolders - label them,
+   - function return data X (images), data y (labels) and dictionary with labels meaning
+"""
 
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
+def load_dataset(dataset_path):
+    data_classes = [f for f in os.listdir(dataset_path) if os.path.isdir(dataset_path + f)]
+    X = []
+    y = []
+    y_dictionary = {}
+    for cl in data_classes:
+        imgs_names = [f for f in os.listdir(dataset_path+cl) if os.path.isfile(dataset_path+cl+'/'+f)]
+        loaded_imgs = np.array([cv2.imread(dataset_path+cl+'/'+img_name, cv2.IMREAD_COLOR) for img_name in imgs_names])
+        class_label = data_classes.index(cl)
+        labels = np.array([class_label for nb in range(0, loaded_imgs.shape[0])])
+        X = np.concatenate((X, loaded_imgs))
+        y = np.concatenate((y, labels))
+        y_dictionary[class_label] = cl
+    return X, y, y_dictionary
