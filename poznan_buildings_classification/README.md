500a54d7fec27499754a366ce43c9a518255534b
m-milena <milenamolska@gmail.com>
2020-04-28 10:41:27 +0200

    Added some scores to test script and edited readme

--- poznan_buildings_classification/README.md
+++ poznan_buildings_classification/README.md
@@ -1,8 +1,23 @@
 # Poznan's buildings classificator
 
 Project from Advanced Image Processing subject to classify 5 popular buildings in Poznan using Bag of Words model.
-## Description
-Action steps:
-1. At first, I create a script to generate a lot of vocabularies with different numbers of words. I used the k-means algorithm and generate 5 models to every single vocabulary with the same number of words. It caused that k-means generate different initial values every time, which gives every time a different set.
+## Requirements
+* opencv-contrib-python 3.4.2.16
+* scikit-learn 0.22.2
+* numpy 1.17.4
 
-Project in progress.
+This project is used SIFT detector, so it is important to have old version of OpenCV.
+
+## Results
+The best results on this train dataset was with image with width resized to 400px. Vocabulary contains 44 words and I used linear SVC model to classify buildings.
+
+Scores on data are presented below:
+| metrics     | Train data          | Test data  |
+| ------------- |:-------------:|:-----:|
+| **accuracy [%]** | 98.73 | 88.33 |
+| **precision [%]**     | 98.75      |  89.14 |
+| **recall [%]** | 98.73    |  88.33 |
+
+Confusion matrix of test data:
+
+![Confusion matrix](https://github.com/m-milena/ML_projects/tree/master/poznan_buildings_classification/visualisation/confusion_matrix.png)
--- poznan_buildings_classification/test_models.py
+++ poznan_buildings_classification/test_models.py
@@ -1,10 +1,11 @@
 import os
-import re
 import cv2
 import pickle
 import numpy as np
 from load_dataset import load_dataset
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
 
 def apply_feature_transform(X, feature_detector_descriptor, vocab_model, nb_words):
     X_transformed = []
@@ -21,39 +22,45 @@ def descriptor2histogram(descriptor, vocab_model, nb_words):
     return histogram
 
 def main():
-    path='./'
-    class_model = './models/classificator.sav'
-    vocab_model = './models/vocabulary.sav'
-    # Info from filename
-    name_pattern = r'vocabulary(?P<number>\d(\d)?)_(?P<nb_words>\d(\d)?)words.sav'
-    regex = re.compile(name_pattern)
+    path='./models/'
+    class_model = 'classificator.sav'
+    vocab_model = 'vocabulary.sav'
     # Load train, test dataset
     X_train, y_train, y_dictionary = load_dataset('./dataset_train/')
     X_test, y_test, _ = load_dataset('./dataset_test/')
+    labels = [y_dictionary.get(key) for key in y_dictionary.keys()]
     width = 400
-    #X_train = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in X_train]
+    X_train = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in X_train]
     X_train = [cv2.resize(img, (width, int(img.shape[0]*width/img.shape[1]))) for img in X_train]
-    #X_test = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in X_test]
+    X_test = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in X_test]
     X_test = [cv2.resize(img, (width, int(img.shape[0]*width/img.shape[1]))) for img in X_test]
     # Create descriptor
-    feature_detector_descriptor =  cv2.xfeatures2d.SIFT_create()#cv2.AKAZE_create()#xfeatures2d.SIFT_create()
+    feature_detector_descriptor =  cv2.xfeatures2d.SIFT_create()
 
-    nb = int(regex.search(vocab_model).group('number'))
-    nb_words = int(regex.search(vocab_model).group('nb_words'))
     clf_kmeans = pickle.load(open(path+vocab_model, 'rb'))
-        # Process dataset
+    nb_words = clf_kmeans.n_clusters
+    # Process dataset
     X_train_trans = apply_feature_transform(X_train, feature_detector_descriptor, clf_kmeans, nb_words)
     X_test_trans = apply_feature_transform(X_test, feature_detector_descriptor, clf_kmeans, nb_words)
 
     clf_svc = pickle.load(open(path+class_model, 'rb'))
     predicted_train = clf_svc.predict(X_train_trans)
     predicted_test = clf_svc.predict(X_test_trans)
-    print(predicted_test)
-    acc_train = accuracy_score(predicted_train, y_train)
+    acc_train = accuracy_score(y_train, predicted_train)
+    prec_train = precision_score(y_train, predicted_train, average='weighted')
+    recall_train = recall_score(y_train, predicted_train, average='weighted')
     acc_test = accuracy_score(predicted_test, y_test) 
-    print('Train accuracy: ', acc_train)
-    print('Test accuracy: ', acc_test) 
+    prec_test = precision_score(y_test, predicted_test, average='weighted')
+    recall_test = recall_score(y_test, predicted_test, average='weighted')
+    print('Train accuracy / precision / recall: ', acc_train, prec_train, recall_train)
+    print('Test accuracy: ', acc_test, prec_test, recall_test) 
     print(confusion_matrix(y_test, predicted_test))
+    disp = plot_confusion_matrix(clf_svc, X_test_trans, y_test, 
+                                    display_labels=labels,
+                                    cmap=plt.cm.YlGn,
+                                    normalize = 'true')
+    plt.show()
+    
     for p in range(0, len(predicted_test)):
         print('Predicted: ', y_dictionary.get(predicted_test[p]))
         print('Correct: ', y_dictionary.get(y_test[p]))
